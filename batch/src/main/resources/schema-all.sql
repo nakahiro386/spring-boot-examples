f8c8e34de2369bcf27fe8a1ea25f991e70cdabec
nakahiro386 <nakahiro386@gmail.com>
2020-03-07 23:54:19 +0900

    h2dbをローカル接続で利用するよう変更

--- batch/src/main/resources/schema-all.sql
+++ batch/src/main/resources/schema-all.sql
@@ -1,6 +1,4 @@
-DROP TABLE people IF EXISTS;
-
-CREATE TABLE people  (
+CREATE TABLE IF NOT EXISTS people  (
     person_id BIGINT IDENTITY NOT NULL PRIMARY KEY,
     first_name VARCHAR(20),
     last_name VARCHAR(20)
