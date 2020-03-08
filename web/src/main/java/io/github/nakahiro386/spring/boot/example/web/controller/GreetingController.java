f733b78deedda613b71b40cbcb09663340243bcc
nakahiro386 <nakahiro386@gmail.com>
2020-03-08 11:14:42 +0900

    入門ガイドのフォーム送信処理実装
    Spring Boot 入門 | フォーム送信処理 - コードサンプル
    https://spring.pleiades.io/guides/gs/handling-form-submission/

--- web/src/main/java/io/github/nakahiro386/spring/boot/example/web/controller/GreetingController.java
+++ web/src/main/java/io/github/nakahiro386/spring/boot/example/web/controller/GreetingController.java
@@ -3,15 +3,22 @@ package io.github.nakahiro386.spring.boot.example.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import io.github.nakahiro386.spring.boot.example.web.form.Greeting;
 
 @Controller
 public class GreetingController {
 
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
-            Model model) {
-        model.addAttribute("name", name);
+    public String greetingForm(Model model) {
+        model.addAttribute("greeting", new Greeting());
         return "greeting";
+    }
+
+    @PostMapping("/greeting")
+    public String greetingSubmit(@ModelAttribute Greeting greeting) {
+        return "result";
     }
 }
---
+++ web/src/main/java/io/github/nakahiro386/spring/boot/example/web/form/Greeting.java
@@ -0,0 +1,9 @@
+package io.github.nakahiro386.spring.boot.example.web.form;
+
+import lombok.Data;
+
+@Data
+public class Greeting {
+    private Long id;
+    private String content;
+}
